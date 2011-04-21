ba1f2a1cd139867ac882d2434d4bc8115f852098
Peter Hilton <pedro+git@lunatech.com>
2011-04-21 19:59:43 +0200

    cosmetic changes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,7 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
+import models.User;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
