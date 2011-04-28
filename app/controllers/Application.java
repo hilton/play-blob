16153c141f4c691047fac20c1bd79ebdb21935ce
Peter Hilton <pedro+git@lunatech.com>
2011-04-28 21:15:37 +0200

    Code improvements, and DateBlob for attachment subdirectories

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.DateBlob;
 import models.User;
 import play.db.jpa.Blob;
 import play.libs.MimeTypes;
@@ -22,14 +23,15 @@ public class Application extends Controller {
 
 	public static void userPhoto(long id) {
 	   final User user = User.findById(id);
+		notFoundIfNull(user);
 	   response.setContentTypeIfNotSet(user.photo.type());
 	   renderBinary(user.photo.get());
 	}
 
 	public static void addUserWithFileName(File photo) throws FileNotFoundException {
-	   final User user = new User();
+		final User user = new User();
 	   user.photoFileName = photo.getName();
-	   user.photo = new Blob();
+	   user.photo = new DateBlob();
 	   user.photo.set(new FileInputStream(photo), MimeTypes.getContentType(photo.getName()));
 	   user.save();
 	   index();
@@ -38,8 +40,20 @@ public class Application extends Controller {
 	public static void downloadUserPhoto(long id) {
 	   final User user = User.findById(id);
 	   response.setContentTypeIfNotSet(user.photo.type());
-	   response.setHeader("Content-Disposition", "attachment; filename=\"" + user.photoFileName + "\"");
-	   renderBinary(user.photo.get());
+	   renderBinary(user.photo.get(), user.photoFileName);
 	}
+
+	public static void updateUser(User user) {
+		user.save();
+		index();
+	}
+
+	public static void deleteUser(long id) {
+		final User user = User.findById(id);
+//		user.photo.getFile().delete();
+		user.delete();
+		index();
+	}
+
 
 }
--- app/models/User.java
+++ app/models/User.java
@@ -4,10 +4,18 @@ import play.db.jpa.Blob;
 import play.db.jpa.Model;
 
 import javax.persistence.Entity;
+import java.io.File;
 
 @Entity
 public class User extends Model {
 
-   public String photoFileName;
-   public Blob photo;
+	public String photoFileName;
+//	public Blob photo;
+	public DateBlob photo;
+
+	@Override
+	public void _delete() {
+		super._delete();
+		photo.getFile().delete();
+	}
 }
